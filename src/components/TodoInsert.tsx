14072cd2587314993bcd561c4bc993a835deebdb
ShinEunJin <sineun5501@naver.com>
2021-11-01 23:47:48 +0900

    onSubmit usecallback

--- src/components/TodoInsert.tsx
+++ src/components/TodoInsert.tsx
@@ -14,8 +14,17 @@ const TodoInsert = ({ onInsert }: TodoInsertProps) => {
     setValue(e.target.value)
   }, [])
 
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault()
+      onInsert(value)
+      setValue("")
+    },
+    [onInsert, value]
+  )
+
   return (
-    <form className="TodoInsert">
+    <form className="TodoInsert" onSubmit={onSubmit}>
       <input
         placeholder="할 일을 입력하세요"
         value={value}
