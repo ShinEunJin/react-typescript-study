a9f27496fafbe0fbe29c4123c865c703b05bdab6
ShinEunJin <sineun5501@naver.com>
2021-11-18 23:52:06 +0900

    react redux typescript studying

--- src/redux/modules/todos.ts
+++ src/redux/modules/todos.ts
@@ -1,63 +1,35 @@
+import { action, createAction, createReducer } from "typesafe-actions"
+
 const CHANGE_INPUT = "todos/CHANGE_INPUT"
 const INSERT = "todos/INSERT"
 const TOGGLE = "todos/TOGGLE"
 const REMOVE = "todos/REMOVE"
 
-export const changeInput = (input: string) => ({
-    type: CHANGE_INPUT,
-    input
-})
+interface IReducer {
+    [CHANGE_INPUT]: 
+}
 
-let id = 3
-export const insert = (text: string) => ({
-    type: INSERT,
-    todo: {
-        id: id++,
-        text,
-        done: false
-    }
-})
+export const changeInput = createAction(CHANGE_INPUT, action => (input: string) => action({ input }))
 
-export const toggle = (id: number) => ({
-    type: TOGGLE,
-    id
-})
+let id = 1
+export const insert = createAction(INSERT, (text: string) => ({
+    id: id++,
+    text,
+    done: false
+}))
 
-export const remove = (id: number) => ({
-    type: REMOVE,
-    id
-})
+export const toggle = createAction(TOGGLE, (id: number) => id)
+
+export const remove = createAction(REMOVE, (id: number) => id)
 
 const initialState: { input: string, todos: { id: number, done: boolean }[] } = {
     input: "",
     todos: []
 }
 
-const todos = (state = initialState, action: { type: string, input: string, todo: {}, id: number }) => {
-    switch (action.type) {
-        case CHANGE_INPUT:
-            return {
-                ...state,
-                input: action.input
-            }
-        case INSERT:
-            return {
-                ...state,
-                todos: [...state.todos, action.todo]
-            }
-        case TOGGLE:
-            return {
-                ...state,
-                todos: state.todos.map(todo => todo.id === action.id ? { ...todo, done: !todo.done } : todo)
-            }
-        case REMOVE:
-            return {
-                ...state,
-                todos: state.todos.filter(todo => todo.id !== action.id)
-            }
-        default:
-            return state
-    }
-}
+const todos = handleActions({
+    [CHANGE_INPUT]: (state, action) => ({ ...state, input: action.payload }),
+
+}, initialState)
 
 export default todos
