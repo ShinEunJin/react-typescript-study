311a53eacd9541fc90fe00b544e6114fbaa811a9
ShinEunJin <sineun5501@naver.com>
2022-02-17 00:25:48 +0900

    팝업창 그리고 쿠키제어

--- src/Modal/Popup.tsx
+++ src/Modal/Popup.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
-import { useCookies } from 'react-cookie'
+import React, { Dispatch, SetStateAction } from 'react'
+import dayjs from 'dayjs'
+import './styles.scss'
 
-const Popup = () => {
+interface PopupProps {
+    setShowPopup: Dispatch<SetStateAction<boolean>>
+    setCookie: (name: "popup-cookie", value: any, options?: { path?: any, expires?: Date }) => void
+}
+
+const Popup = ({ setShowPopup, setCookie }: PopupProps) => {
+
+    const onClosePopup = () => {
+        setShowPopup(false)
+        setCookie("popup-cookie", true, { path: '/', expires: dayjs().add(1, 'day').toDate() })
+    }
+
     return (
-        <div>popup</div>
+        <div className='popup-box'><span onClick={onClosePopup}>닫기(쿠키 1일 등록)</span></div>
     )
 }
 
--- src/Modal/index.tsx
+++ src/Modal/index.tsx
@@ -1,13 +1,17 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
+import { useCookies } from 'react-cookie'
+
 import Popup from './Popup'
 
 const Index = () => {
 
     const [showPopup, setShowPopup] = useState(true)
 
+    const [cookies, setCookie] = useCookies(['popup-cookie'])
+
     return (
         <div>
-            {showPopup && <Popup />}
+            {showPopup && !cookies['popup-cookie'] && <Popup setShowPopup={setShowPopup} setCookie={setCookie} />}
         </div>
     )
 }
